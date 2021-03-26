3e04fa863b6a9141d5c432a262a6d8d95d4e3cf8
b3457m0d3 <perchance2dream86@gmail.com>
2021-03-26 09:02:23 -0400

    clean up

--- client/src/components/component_template_props.js
+++ client/src/components/component_template_props.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from "react"
 import PropTypes from 'prop-types'
 
 function ChangeMe(props) {
-  const { index, ...other } = props
+  const { index, ...other } = props //access the prop values 
   const [val,setVal] = useState('')
 
   useEffect(() => {
     
-  },[])//adding the array as a 2nd parameter ensures the effect is applied once per rendernpm run dev
-  
+  },[])//adding the array as a 2nd parameter ensures the effect is applied once per render
   return ('')
   
 }
-ChangeMe.propTypes = {
+ChangeMe.propTypes = { //assurance that data passed in is the correct type
   index: PropTypes.any.isRequired,
 };
 
