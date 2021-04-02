73e1c27681c803e3ec373b29f9d19e73d414e446
b3457m0d3 <perchance2dream86@gmail.com>
2021-04-01 22:51:54 -0400

    refactored RPMGauge using custom socket.io hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import FullWidthTabs from './components/FullWidthTabs'
-import Login from './components/Auth/Login'
+import RPMGauge from './components/RPMGauge'
 
 function App(){
   return (
-    <Login />
+    <RPMGauge />
   )
   
 }
--- client/src/components/RPMGauge.js
+++ client/src/components/RPMGauge.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from "react"
-import socketIOClient from "socket.io-client"
+import useSocket from "use-socket.io-client"
 import ReactSpeedometer from "react-d3-speedometer"
-
-function SerialSocket() {
-  const ENDPOINT = "http://localhost:4001"
+function RPMGauge() {
   const [serialData,setSerialData] = useState()
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT)
-    socket.on('data', (data) => {
-      setSerialData(data)
-    })
-  },[])
-  
+  const [socket] = useSocket('http://localhost:4001',{ autoConnect: true, })
+  socket.on('data', (data) => { setSerialData(data) })
   return (
     <ReactSpeedometer
       width={200}
@@ -20,6 +12,5 @@ function SerialSocket() {
       value={serialData}
     />
   )
-  
 }
-export default SerialSocket
+export default RPMGauge
